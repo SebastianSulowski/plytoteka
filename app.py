99a734d7ec5ffd779ba1fb896f8084d8f05d2a6c
SebastianSulowski <116625989+SebastianSulowski@users.noreply.github.com>
2023-01-30 15:22:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,49 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, FileField
-from wtforms.validators import InputRequired
+from flask import Flask, request
 from flask_restful import Resource, Api
-from werkzeug.utils import secure_filename
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
 api = Api(app)
 
-class AlbumForm(FlaskForm):
-    title = StringField('Title', validators=[InputRequired()])
-    cover = FileField('Cover')
-
-@app.route('/')
-def index():
-    form = AlbumForm()
-    return render_template('index.html', form=form)
-
-@app.route('/albums', methods=['POST'])
-def add_album():
-    form = AlbumForm()
-    if form.validate_on_submit():
-        title = form.title.data
-        cover = form.cover.data
-
-        filename = secure_filename(cover.filename)
-        cover.save(os.path.join('static/img', filename))
-
-        return redirect(url_for('index'))
+albums = []
 
 class Album(Resource):
     def get(self, id):
-        pass
-
+        for album in albums:
+            if album['id'] == id:
+                return album
+        return {"error": "album not found"}, 404
+    
     def put(self, id):
-        pass
-
+        for album in albums:
+            if album['id'] == id:
+                album['title'] = request.form['title']
+                album['artist'] = request.form['artist']
+                return {"message": "album updated successfully"}
+        return {"error": "album not found"}, 404
+    
     def delete(self, id):
-        pass
+        for album in albums:
+            if album['id'] == id:
+                albums.remove(album)
+                return {"message": "album deleted successfully"}
+        return {"error": "album not found"}, 404
+
+class AlbumList(Resource):
+    def get(self):
+        return albums
+    
+    def post(self):
+        album = {
+            'id': len(albums) + 1,
+            'title': request.form['title'],
+            'artist': request.form['artist']
+        }
+        albums.append(album)
+        return {"message": "album added successfully", "album": album}, 201
 
 api.add_resource(Album, '/album/<int:id>')
+api.add_resource(AlbumList, '/albums')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
